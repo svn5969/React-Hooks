d7b2363fd909fbcc7ae6cf804b2f960e8ddb67ea
svn5969 <sourav.svn.10@gmail.com>
2021-10-30 00:52:03 +0600

    add ttry catch & props in UseeffectApi

--- src/components/useEffect Hook/UseEffectApi.js
+++ src/components/useEffect Hook/UseEffectApi.js
@@ -1,50 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
 import './useEffectApi.css'
+import Users from './Users';
 const UseEffectApi = () => {
     const [users,setUsers] = useState([])
+    const [loading,setLoading] = useState(true)
     const getUsers = async () => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+       try {
+        setLoading(false)
+        const response = await fetch('https://api.github.com/users')
         setUsers(await response.json());
+       } catch (error) {
+        setLoading(false)
+           console.log(error);
+       }
         
 
     }
     useEffect(()=>{
         getUsers();
+        
     },[])
+    
+    if (loading) {
+        return <Loading></Loading>
+        
+    }
+
     return (
         <>
-          <h2>List of GitHub Users</h2>
-          <div className="container-fluid mt-5">
-        <div className="row text-center">
-        {
-            users.map((curElem)=>{
-             return (
-                
-                 <div className="col-10 col-md-6 mt-5" key={curElem.id}>
-             <div className="card p-2">
-              <div className="d-flex align-items-center">
-                  <div className="image">
-                  <img src={curElem.thumbnailUrl} alt="" className="rounded" width="155" />
-                  </div>
-                  <div className="ml-3 w-100">
-                      <h4 className="mb-0 mt-0 textLeft">{curElem.title}</h4> <span className="textLeft"></span>
-                      <div className="justify-content-between p-2 mt-2 bg-primary d-flex text-white stats">
-                          <div className="d-flex flex-column"> <span className="articles">Articles</span> <span className="number1">38</span> </div>
-                          <div className="d-flex flex-column"> <span className="followers">Followers</span>  <span className="number2">980</span></div>
-                          <div className="d-flex flex-column"> <span className="rating">Rating</span> <span className="number3">9.8</span> </div>
-                          
-                      </div>
-                  </div>
-              </div>
-             </div>
-           </div>
-                
-             )
-            })
-        }
-          
-        </div>
-          </div>
+          <Users users={users}></Users>
         </>
     );
 };
---
+++ src/components/useEffect Hook/Users.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Users = ({users}) => {
+    
+    return (
+        <div>
+            <h2>List of GitHub Users</h2>
+          <div className="container-fluid mt-5">
+        <div className="row text-center">
+        {
+            users.map((curElem)=>{
+                const {avatar_url,id,login,type} = curElem;
+             return (
+                
+                 <div className="col-10 col-md-6 mt-5" key={id}>
+             <div className="card p-2">
+              <div className="d-flex align-items-center">
+                  <div className="image">
+                  <img src={avatar_url} alt="" className="rounded" width="155" />
+                  </div>
+                  <div className="ml-3 w-100">
+                      <h4 className="mb-0 mt-0 textLeft">{login}</h4> <span className="textLeft">{type}</span>
+                      <div className="justify-content-between p-2 mt-2 bg-primary d-flex text-white stats">
+                          <div className="d-flex flex-column"> <span className="articles">Articles</span> <span className="number1">38</span> </div>
+                          <div className="d-flex flex-column"> <span className="followers">Followers</span>  <span className="number2">980</span></div>
+                          <div className="d-flex flex-column"> <span className="rating">Rating</span> <span className="number3">9.8</span> </div>
+                          
+                      </div>
+                  </div>
+              </div>
+             </div>
+           </div>
+                
+             )
+            })
+        }
+          
+        </div>
+          </div>
+        </div>
+    );
+};
+
+export default Users;
