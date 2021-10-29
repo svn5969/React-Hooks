d87ad716d264dbe15ba41effe43d892ddf788206
svn5969 <sourav.svn.10@gmail.com>
2021-10-29 12:52:42 +0600

    add UseEffectApi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShortCircuitEvaluation from './components/ShortCircuitEvaluation';
 import BasicForms from './components/forms/BasicForms';
 import UseEffect1 from './components/useEffect Hook/UseEffect1';
 import UseEffect2 from './components/useEffect Hook/UseEffect2';
+import UseEffectApi from './components/useEffect Hook/UseEffectApi';
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
      {/* <ShortCircuitEvaluation></ShortCircuitEvaluation> */}
      {/* <BasicForms></BasicForms> */}
      {/* <UseEffect1></UseEffect1> */}
-     <UseEffect2></UseEffect2>
+     {/* <UseEffect2></UseEffect2> */}
+     <UseEffectApi></UseEffectApi>
     </div>
   );
 }
